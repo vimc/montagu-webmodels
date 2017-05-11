5c7edf1755601b5d2288fc86ae518e9024c4cd1f
Martin Eden <martin.s.eden@gmail.com>
2017-05-11 16:30:36 +0100

    Add CoverageSet

---
+++ src/main/kotlin/org/vaccineimpact/api/models/Coverage.kt
@@ -0,0 +1,15 @@
+package org.vaccineimpact.api.models
+
+data class CoverageSet(
+        override val id: Int,
+        val touchstone: String,
+        val name: String,
+        val vaccine: String,
+        val gaviSupportLevel: String,
+        val activityType: String
+): HasKey<Int>
+
+data class ScenarioAndCoverageSets(
+        val scenario: Scenario,
+        val coverageSets: List<CoverageSet>
+)
