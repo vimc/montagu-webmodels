38e297632d284bfea76acf5bff7758638b7c6914
Martin Eden <martin.s.eden@gmail.com>
2017-05-22 17:16:56 +0100

    Add CoverageRow

--- src/main/kotlin/org/vaccineimpact/api/models/Coverage.kt
+++ src/main/kotlin/org/vaccineimpact/api/models/Coverage.kt
@@ -1,5 +1,7 @@
 package org.vaccineimpact.api.models
 
+import java.math.BigDecimal
+
 data class CoverageSet(
         override val id: Int,
         val touchstone: String,
@@ -18,4 +20,20 @@ data class ScenarioTouchstoneAndCoverageSets(
         val touchstone: Touchstone,
         val scenario: Scenario,
         val coverageSets: List<CoverageSet>
+)
+
+data class CoverageRow(
+        val setId: Int,
+        val setOrder: Int,
+        val setName: String,
+        val vaccine: String,
+        val gaviSupportLevel: GAVISupportLevel,
+        val activityType: ActivityType,
+        val country: String,
+        val year: Int,
+        val ageFrom: BigDecimal?,
+        val ageTo: BigDecimal?,
+        val ageRangeVerbatim: String?,
+        val target: BigDecimal?,
+        val coverage: BigDecimal?
 )
