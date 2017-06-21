5a8132172eaf2d09fe3f36afa88ade262a4b56d1
Alex Hill <alex.hill@gmail.com>
2017-06-21 17:29:29 +0100

    new interface for userdto which both minimal user info and user info plus roles inherit from

--- src/main/kotlin/org/vaccineimpact/api/models/UserDto.kt
+++ src/main/kotlin/org/vaccineimpact/api/models/UserDto.kt
@@ -3,10 +3,24 @@ package org.vaccineimpact.api.models
 import org.vaccineimpact.api.models.permissions.RoleAssignment
 import java.sql.Timestamp
 
+interface UserDtoInterface{
+        val username: String
+        val name: String
+        val email: String
+        val lastLoggedIn: Timestamp?
+}
+
 data class UserDto(
-        val username: String,
-        val name: String,
-        val email: String,
-        val lastLoggedIn: Timestamp?,
+        override val username: String,
+        override val name: String,
+        override val email: String,
+        override val lastLoggedIn: Timestamp?
+): UserDtoInterface
+
+data class UserWithRolesDto(
+        override val username: String,
+        override val name: String,
+        override val email: String,
+        override val lastLoggedIn: Timestamp?,
         val roles: List<RoleAssignment>
-)
+) : UserDtoInterface
