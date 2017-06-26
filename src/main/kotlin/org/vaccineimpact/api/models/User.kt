2dce240e8c075dd0cdb938a7eb9c966c79ace4b4
Martin Eden <martin.s.eden@gmail.com>
2017-06-26 16:15:52 +0100

    Add secondary constructor to User

--- src/main/kotlin/org/vaccineimpact/api/models/User.kt
+++ src/main/kotlin/org/vaccineimpact/api/models/User.kt
@@ -9,3 +9,8 @@ data class User(
         val email: String,
         val lastLoggedIn: Timestamp?,
         val roles: List<RoleAssignment>?)
+{
+    // Exists to make it easier to map with jOOQ
+    constructor(username: String, name: String, email: String, lastLoggedIn: Timestamp?)
+        : this(username, name, email, lastLoggedIn, roles = null)
+}
