25e7f377c3044f37290e09877b6979a960942fb9
Martin Eden <martin.s.eden@gmail.com>
2017-06-26 15:19:42 +0100

    Merge User models into one with nullable Roles

--- src/main/kotlin/org/vaccineimpact/api/models/User.kt
+++ src/main/kotlin/org/vaccineimpact/api/models/User.kt
@@ -3,23 +3,9 @@ package org.vaccineimpact.api.models
 import org.vaccineimpact.api.models.permissions.RoleAssignment
 import java.sql.Timestamp
 
-interface UserInterface{
-        val username: String
-        val name: String
-        val email: String
-        val lastLoggedIn: Timestamp?
-}
-
 data class User(
-        override val username: String,
-        override val name: String,
-        override val email: String,
-        override val lastLoggedIn: Timestamp?
-): UserInterface
-
-data class UserWithRoles(
-        override val username: String,
-        override val name: String,
-        override val email: String,
-        override val lastLoggedIn: Timestamp?,
-        var roles: List<RoleAssignment>) : UserInterface
+        val username: String,
+        val name: String,
+        val email: String,
+        val lastLoggedIn: Timestamp?,
+        var roles: List<RoleAssignment>?)
