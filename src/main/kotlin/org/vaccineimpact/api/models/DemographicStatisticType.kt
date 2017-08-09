1c1d1dddb3a98ea3b625645baae6cdaa6a3dc050
Alex Hill <alex.hill@gmail.com>
2017-08-09 18:30:51 +0100

    remove variants from model

--- src/main/kotlin/org/vaccineimpact/api/models/DemographicStatisticType.kt
+++ src/main/kotlin/org/vaccineimpact/api/models/DemographicStatisticType.kt
@@ -3,7 +3,6 @@ package org.vaccineimpact.api.models
 data class DemographicStatisticType(
         override val id: String,
         val name: String,
-        val variants: List<String>,
         val genderIsApplicable: Boolean,
         val countries: List<String>,
         val source: String) : HasKey<String>
