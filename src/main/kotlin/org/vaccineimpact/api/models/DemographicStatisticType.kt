0053346c487dc8f7b1bd2dbc993a4406702c0081
Alex Hill <alex.hill@gmail.com>
2017-08-09 18:23:03 +0100

    extra models for demographic data

--- src/main/kotlin/org/vaccineimpact/api/models/DemographicStatisticType.kt
+++ src/main/kotlin/org/vaccineimpact/api/models/DemographicStatisticType.kt
@@ -1,5 +1,7 @@
 package org.vaccineimpact.api.models
 
+import java.math.BigDecimal
+
 data class DemographicStatisticType(
         override val id: String,
         val name: String,
@@ -7,3 +9,26 @@ data class DemographicStatisticType(
         val genderIsApplicable: Boolean,
         val countries: List<String>,
         val source: String) : HasKey<String>
+
+data class DemographicDataset(
+        override val id: String,
+        val name: String,
+        val variant: String,
+        val gender: String,
+        val countries: List<String>,
+        val unit: String,
+        val ageInterpretation: String,
+        val source: String) : HasKey<String>
+
+data class DemographicDataForTouchstone(
+        val touchstone: Touchstone,
+        val demographicData: DemographicStatisticType?
+)
+
+data class DemographicRow(
+        val country: String,
+        val age: String,
+        val year: Int,
+        val gender: String,
+        val value: BigDecimal
+)
