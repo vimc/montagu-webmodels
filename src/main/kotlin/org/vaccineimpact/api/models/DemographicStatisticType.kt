cba4367840622deca7d0c522115ff3512b6fb67d
Alex Hill <alex.hill@gmail.com>
2017-10-13 18:19:34 +0100

    wip

--- src/main/kotlin/org/vaccineimpact/api/models/DemographicStatisticType.kt
+++ src/main/kotlin/org/vaccineimpact/api/models/DemographicStatisticType.kt
@@ -33,3 +33,22 @@ data class DemographicRow(
         val gender: String,
         val value: BigDecimal
 )
+
+data class WideDemographicRow(
+        val countryCodeNumeric: Int,
+        val countryCode: String,
+        val country: String,
+        val ageFrom: Int,
+        val ageTo: Int,
+        val gender: String,
+        val valuesPerYear: Map<String, BigDecimal>
+)
+
+data class WideDemographicData(override val rows: List<WideDemographicRow>,
+                               override val flexibleHeaders: List<String>) : FlexibleData<WideDemographicRow>
+
+interface FlexibleData<T>
+{
+    val rows: Iterable<T>
+    val flexibleHeaders: Iterable<String>
+}
