8c36ded08c59c1792f9cbab8165f5791ad2b0154
Alex Hill <alex.hill@gmail.com>
2017-11-16 16:39:15 +0000

    wip

--- src/main/kotlin/org/vaccineimpact/api/models/ModelRunParameterSet.kt
+++ src/main/kotlin/org/vaccineimpact/api/models/ModelRunParameterSet.kt
@@ -1,12 +1,14 @@
 package org.vaccineimpact.api.models
 
+import org.vaccineimpact.api.models.helpers.FlexibleColumns
+
 data class ModelRunParameterSet(
         val responsibilitySet: Int,
         val description: String,
         val uploadInfo: UploadInfo,
         val modelVersion: Int,
-        val parameters: List<String>,
         val modelRuns: List<ModelRun>
 )
 
+@FlexibleColumns
 data class ModelRun(val runId: String, val parameterValues: Map<String, String>)
