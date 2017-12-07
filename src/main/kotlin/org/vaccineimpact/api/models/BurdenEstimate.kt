ec97dfa4a0f3856c104996783f87a0c852330cfb
Martin Eden <martin.s.eden@gmail.com>
2017-12-07 15:16:43 +0000

    Add BurdenEstimateWithRunId

--- src/main/kotlin/org/vaccineimpact/api/models/BurdenEstimate.kt
+++ src/main/kotlin/org/vaccineimpact/api/models/BurdenEstimate.kt
@@ -13,3 +13,22 @@ data class BurdenEstimate(
         val cohortSize: BigDecimal,
         val outcomes: Map<String, BigDecimal?>
 )
+
+@FlexibleColumns
+data class BurdenEstimateWithRunId(
+        val disease: String,
+        val runId: String?,
+        val year: Int,
+        val age: Int,
+        val country: String,
+        val countryName: String,
+        val cohortSize: BigDecimal,
+        val outcomes: Map<String, BigDecimal?>
+)
+{
+    constructor(estimate: BurdenEstimate, runId: String?) : this(
+            estimate.disease, runId,
+            estimate.year, estimate.age, estimate.country, estimate.countryName,
+            estimate.cohortSize, estimate.outcomes
+    )
+}
