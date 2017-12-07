b23b1b1a899c81278c18c3512af4fd80a283db33
Martin Eden <martin.s.eden@gmail.com>
2017-12-07 16:03:45 +0000

    Distinguish between internal format and external format

--- src/main/kotlin/org/vaccineimpact/api/models/BurdenEstimate.kt
+++ src/main/kotlin/org/vaccineimpact/api/models/BurdenEstimate.kt
@@ -15,6 +15,17 @@ data class BurdenEstimate(
 )
 
 @FlexibleColumns
+data class StochasticBurdenEstimate(
+        val disease: String,
+        val runId: String,
+        val year: Int,
+        val age: Int,
+        val country: String,
+        val countryName: String,
+        val cohortSize: BigDecimal,
+        val outcomes: Map<String, BigDecimal?>
+)
+
 data class BurdenEstimateWithRunId(
         val disease: String,
         val runId: String?,
@@ -28,6 +39,11 @@ data class BurdenEstimateWithRunId(
 {
     constructor(estimate: BurdenEstimate, runId: String?) : this(
             estimate.disease, runId,
+            estimate.year, estimate.age, estimate.country, estimate.countryName,
+            estimate.cohortSize, estimate.outcomes
+    )
+    constructor(estimate: StochasticBurdenEstimate) : this(
+            estimate.disease, estimate.runId,
             estimate.year, estimate.age, estimate.country, estimate.countryName,
             estimate.cohortSize, estimate.outcomes
     )
