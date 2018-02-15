c5dfb8f4da3350f345da4307baca5d4fa9d18d5a
Alex Hill <alex.hill@gmail.com>
2018-02-15 17:18:31 +0000

    add properties to report

--- src/main/kotlin/org/vaccineimpact/api/models/Report.kt
+++ src/main/kotlin/org/vaccineimpact/api/models/Report.kt
@@ -1,7 +1,12 @@
 package org.vaccineimpact.api.models
 
 import java.beans.ConstructorProperties
+import java.time.Instant
 
 data class Report
-@ConstructorProperties("name", "displayname", "latestVersion")
-constructor(val name: String, val displayName: String?, val latestVersion: String)
+@ConstructorProperties("name", "displayname", "latestVersion", "published", "date")
+constructor(val name: String,
+            val displayName: String?,
+            val latestVersion: String,
+            val published: Boolean,
+            val lastGenerated: Instant)
