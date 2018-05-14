3137d2e7b1ddc0c0111ce08acdee365a1ded5eb9
Alex Hill <alex.hill@gmail.com>
2018-05-14 13:21:21 +0100

    add more metdata to report model

--- src/main/kotlin/org/vaccineimpact/api/models/Report.kt
+++ src/main/kotlin/org/vaccineimpact/api/models/Report.kt
@@ -1,10 +1,15 @@
 package org.vaccineimpact.api.models
 
 import java.beans.ConstructorProperties
+import java.time.Instant
 
 data class Report
-@ConstructorProperties("name", "displayname", "latestVersion", "published")
+@ConstructorProperties("name", "displayname", "latestVersion", "published", "creationDate", "author",
+        "requester")
 constructor(val name: String,
             val displayName: String?,
             val latestVersion: String,
-            val published: Boolean)
+            val published: Boolean,
+            val creationDate: Instant,
+            val author: String?,
+            val requester: String?)
