d443473c570ee5cb578ce40f8775284dcb1fed35
Alex Hill <alex.hill@gmail.com>
2018-06-15 12:26:56 +0100

    add new model

--- src/main/kotlin/org/vaccineimpact/api/models/ModellingGroup.kt
+++ src/main/kotlin/org/vaccineimpact/api/models/ModellingGroup.kt
@@ -5,7 +5,12 @@ import org.vaccineimpact.api.models.permissions.Username
 data class ModellingGroup(override val id: String,
                           val description: String) : HasKey<String>
 
+data class ModellingGroupCreation(override val id: String,
+                                  val description: String,
+                                  val institution: String,
+                                  val pi: String) : HasKey<String>
+
 data class ModellingGroupDetails(override val id: String,
                                  val description: String,
                                  val models: List<ResearchModel>,
-                                 val members: List<Username>): HasKey<String>
+                                 val members: List<Username>) : HasKey<String>
