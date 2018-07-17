cc7bda4a8ffe9ec8f153544c331edde44c5d5c3c
Alex Hill <alex.hill@gmail.com>
2018-07-17 16:51:16 +0100

    flesh out expectations class

--- src/main/kotlin/org/vaccineimpact/api/models/Expectations.kt
+++ src/main/kotlin/org/vaccineimpact/api/models/Expectations.kt
@@ -1,37 +1,50 @@
 package org.vaccineimpact.api.models
 
-data class Expectations(
+class Expectations(
         val years: IntRange,
         val ages: IntRange,
         val cohorts: CohortRestriction,
         val countries: List<Country>,
-        val outcomes: List<String>
-)
+        val outcomes: List<String>)
+{
+    private fun Int.withinCohortRange(): Boolean
+    {
+        return (cohorts.minimumBirthYear == null || this >= cohorts.minimumBirthYear)
+                && (cohorts.maximumBirthYear == null || this <= cohorts.maximumBirthYear)
+    }
+
+    fun toSequence(): Sequence<BurdenEstimate>
+    {
+        var numCohorts = 0
+
+        for (age in ages)
+        {
+            years
+                    .map { it - age }
+                    .filter { it.withinCohortRange() }
+                    .forEach { numCohorts++ }
+        }
+
+        val numCountries = countries.count()
+        var numRows = numCohorts * numCountries
+        val outcomes = outcomes.associateBy({ it }, { 0F })
+
+        return generateSequence {
+            (numRows--)
+            if (numRows < 0)
+            {
+                null // terminate sequence
+            }
+            else
+            {
+                BurdenEstimate("", 0, 0, "", "", 0F, outcomes)
+            }
+        }
+    }
+
+}
 
 data class CohortRestriction(
         val minimumBirthYear: Short? = null,
         val maximumBirthYear: Short? = null
 )
-
-fun Expectations.toSequence(): Sequence<BurdenEstimate>
-{
-    val numYears = this.years.count()
-    val numAges = this.ages.count()
-    val numCountries = this.countries.count()
-
-    var numRows = numYears * numAges * numCountries
-
-    val outcomes = this.outcomes.associateBy({ it }, { 0F })
-
-    return generateSequence {
-        (numRows--)
-        if (numRows < 0)
-        {
-            null // terminate sequence
-        }
-        else
-        {
-            BurdenEstimate("", 0, 0, "", "", 0F, outcomes)
-        }
-    }
-}
