2fd8efce2f991274059c84fa1ecf7e7bcf334087
Martin Eden <martin.s.eden@pm.me>
2018-07-19 13:47:29 +0100

    Add ResponsibilitySetWithExpectations

--- src/main/kotlin/org/vaccineimpact/api/models/responsibilities/ResponsibilitySet.kt
+++ src/main/kotlin/org/vaccineimpact/api/models/responsibilities/ResponsibilitySet.kt
@@ -2,7 +2,6 @@ package org.vaccineimpact.api.models.responsibilities
 
 data class ResponsibilitySet(
         val modellingGroupId: String,
-        val touchstoneVersion: String,
         val status: ResponsibilitySetStatus,
         val responsibilities: List<Responsibility>
 )
