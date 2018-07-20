ec3f621d400801d50874dfc26d26cad6b237f541
Martin Eden <martin.s.eden@pm.me>
2018-07-20 15:52:38 +0100

    Remove un-implemented field

--- src/main/kotlin/org/vaccineimpact/api/models/responsibilities/ResponsibilitySet.kt
+++ src/main/kotlin/org/vaccineimpact/api/models/responsibilities/ResponsibilitySet.kt
@@ -5,7 +5,6 @@ data class ResponsibilitySet(
         val modellingGroupId: String,
         val status: ResponsibilitySetStatus?,
         val responsibilities: List<Responsibility>
-        //val problems: String = null # Not implemented yet
 ) : Iterable<Responsibility>
 {
     override fun iterator() = responsibilities.iterator()
