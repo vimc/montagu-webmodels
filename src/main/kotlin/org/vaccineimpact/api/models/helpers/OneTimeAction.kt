96a74adaa8aa94e2508feb19e18ac355ec640c6d
Alex Hill <alex.hill@gmail.com>
2018-08-15 15:49:30 +0100

    remove unused enum values

--- src/main/kotlin/org/vaccineimpact/api/models/helpers/OneTimeAction.kt
+++ src/main/kotlin/org/vaccineimpact/api/models/helpers/OneTimeAction.kt
@@ -2,10 +2,5 @@ package org.vaccineimpact.api.models.helpers
 
 enum class OneTimeAction
 {
-    COVERAGE,
-    DEMOGRAPHY,
-    SET_PASSWORD,
-    BURDENS_POPULATE,
-
-    MODEl_RUN_PARAMETERS
+    SET_PASSWORD
 }
