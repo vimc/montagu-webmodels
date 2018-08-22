9eda0c635cbfd6fe0967e8d1a228073a329ce986
Alex Hill <alex.hill@gmail.com>
2018-08-22 17:44:05 +0100

    differentiate reports and versions

--- src/main/kotlin/org/vaccineimpact/api/models/Report.kt
+++ src/main/kotlin/org/vaccineimpact/api/models/Report.kt
@@ -4,6 +4,17 @@ import java.beans.ConstructorProperties
 import java.time.Instant
 
 data class Report
+@ConstructorProperties("name", "displayname", "latestVersion", "published", "updatedOn", "author",
+        "requester")
+constructor(val name: String,
+            val displayName: String?,
+            val latestVersion: String,
+            val published: Boolean,
+            val updatedOn: Instant,
+            val author: String,
+            val requester: String)
+
+data class ReportVersion
 @ConstructorProperties("name", "displayname", "id", "latestVersion", "published", "updatedOn", "author",
         "requester")
 constructor(val name: String,
