4f4ff3727bc5b6fc7515b3a4523c7100c629cb93
Alex Hill <alex.hill@gmail.com>
2018-11-07 16:52:43 +0000

    create new map each time

--- src/main/kotlin/org/vaccineimpact/api/models/Expectations.kt
+++ src/main/kotlin/org/vaccineimpact/api/models/Expectations.kt
@@ -42,15 +42,15 @@ data class Expectations(
     fun expectedRowHashMap(): HashMap<String, HashMap<Int, HashMap<Int, Boolean>>>
     {
         val map = HashMap<String, HashMap<Int, HashMap<Int, Boolean>>>()
-        val ageMap = HashMap<Int, HashMap<Int, Boolean>>()
-        for (age in ages)
-        {
-            val yearMap = HashMap<Int, Boolean>()
-            years.map { if ((it - age).withinCohortRange()) yearMap[it] = false }
-            ageMap[age] = yearMap
-        }
         for (country in countries)
         {
+            val ageMap = HashMap<Int, HashMap<Int, Boolean>>()
+            for (age in ages)
+            {
+                val yearMap = HashMap<Int, Boolean>()
+                years.map { if ((it - age).withinCohortRange()) yearMap[it] = false }
+                ageMap[age] = yearMap
+            }
             map[country.id] = ageMap
         }
         return map
