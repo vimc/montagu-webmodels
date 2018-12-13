b14ba1fadfdd3506bd012fd0f3ca20243a92cb3b
Alex Hill <alex.hill@gmail.com>
2018-12-13 12:04:34 +0000

    make citation nullable

--- src/main/kotlin/org/vaccineimpact/api/models/ResearchModel.kt
+++ src/main/kotlin/org/vaccineimpact/api/models/ResearchModel.kt
@@ -2,5 +2,5 @@ package org.vaccineimpact.api.models
 
 data class ResearchModel(override val id: String,
                          val description: String,
-                         val citation: String,
+                         val citation: String?,
                          val modellingGroup: String): HasKey<String>
