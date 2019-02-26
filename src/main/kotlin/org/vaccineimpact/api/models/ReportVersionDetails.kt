d666c815cf88a73715266246d531e8fe05af2627
Alex <alex.hill@gmail.com>
2019-02-26 15:33:16 +0000

    merged master

--- src/main/kotlin/org/vaccineimpact/api/models/ReportVersionDetails.kt
+++ src/main/kotlin/org/vaccineimpact/api/models/ReportVersionDetails.kt
@@ -1,19 +1,16 @@
 package org.vaccineimpact.api.models
 
-import java.beans.ConstructorProperties
 import java.time.Instant
 
-data class ReportVersionDetails
-@ConstructorProperties("name", "displayname", "id", "published", "date", "author",
-        "requester", "description", "comment", "script", "hashScript")
-constructor(val name: String,
-            val displayName: String?,
-            val id: String,
-            val published: Boolean,
-            val date: Instant,
-            val author: String,
-            val requester: String,
-            val description: String,
-            val comment: String,
-            val script: String?,
-            val hashScript: String? )
+data class ReportVersionDetails(val name: String,
+                                val displayName: String?,
+                                val id: String,
+                                val published: Boolean,
+                                val date: Instant,
+                                val author: String,
+                                val requester: String,
+                                val description: String?,
+                                val artefacts: List<Artefact>,
+                                val resources: List<String>,
+                                val dataHashes: Map<String, String>)
+
