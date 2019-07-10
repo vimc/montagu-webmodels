9775e51c7e5103b266d4cfe8275edff3629b41ee
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-07-10 17:37:12 +0100

    Added TouchstoneModelExpection

---
+++ src/main/kotlin/org/vaccineimpact/api/models/TouchstoneModelExpectation.kt
@@ -0,0 +1,10 @@
+package org.vaccineimpact.api.models
+
+import java.beans.ConstructorProperties
+
+data class TouchstoneModelExpectations
+@ConstructorProperties("touchstoneVersion", "modellingGroup", "disease", "expectations")
+constructor(val touchstoneVersion: String,
+            val modellingGroup: String,
+            val disease: String,
+            val expectations: Expectations)
