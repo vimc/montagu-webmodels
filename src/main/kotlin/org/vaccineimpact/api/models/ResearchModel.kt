ccd45b13570b8f0d9f01978f7aad7ffcf7a13e43
Alex <alex.hill@gmail.com>
2019-08-05 16:44:40 +0100

    distinguihs simpler and detailed models

--- src/main/kotlin/org/vaccineimpact/api/models/ResearchModel.kt
+++ src/main/kotlin/org/vaccineimpact/api/models/ResearchModel.kt
@@ -1,18 +1,20 @@
 package org.vaccineimpact.api.models
 
-import org.vaccineimpact.api.models.helpers.Rule
-import org.vaccineimpact.api.models.helpers.SerializationRule
 import java.beans.ConstructorProperties
 
 data class ResearchModel
+@ConstructorProperties("id", "description", "citation", "modellingGroup")
+constructor(override val id: String,
+            val description: String,
+            val citation: String,
+            val modellingGroup: String) : HasKey<String>
+
+data class ResearchModelDetails
 @ConstructorProperties("id", "description", "citation", "modellingGroup", "genderSpecific", "gender")
 constructor(override val id: String,
             val description: String,
             val citation: String,
             val modellingGroup: String,
-            @SerializationRule(Rule.EXCLUDE_IF_NULL)
             val genderSpecific: Boolean? = null,
-            @SerializationRule(Rule.EXCLUDE_IF_NULL)
             val gender: String? = null,
-            @SerializationRule(Rule.EXCLUDE_IF_NULL)
             var currentVersion: ModelVersion? = null) : HasKey<String>
