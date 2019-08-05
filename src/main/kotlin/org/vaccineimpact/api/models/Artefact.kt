b650b38bac2adba9b6a7ce5a86a3dcbf1dd014b2
Alex <alex.hill@gmail.com>
2019-08-05 15:44:10 +0100

    combine model and researchmodel, remove report related models

--- src/main/kotlin/org/vaccineimpact/api/models/Artefact.kt
+++
@@ -1,3 +0,0 @@
-package org.vaccineimpact.api.models
-
-data class Artefact(val format: ArtefactFormat, val description: String, val files: List<String>)
--- src/main/kotlin/org/vaccineimpact/api/models/ResearchModel.kt
+++ src/main/kotlin/org/vaccineimpact/api/models/ResearchModel.kt
@@ -1,6 +1,18 @@
 package org.vaccineimpact.api.models
 
-data class ResearchModel(override val id: String,
-                         val description: String,
-                         val citation: String?,
-                         val modellingGroup: String): HasKey<String>
+import org.vaccineimpact.api.models.helpers.Rule
+import org.vaccineimpact.api.models.helpers.SerializationRule
+import java.beans.ConstructorProperties
+
+data class ResearchModel
+@ConstructorProperties("id", "description", "citation", "modellingGroup", "genderSpecific", "gender")
+constructor(override val id: String,
+            val description: String,
+            val citation: String,
+            val modellingGroup: String,
+            @SerializationRule(Rule.EXCLUDE_IF_NULL)
+            val genderSpecific: Boolean? = null,
+            @SerializationRule(Rule.EXCLUDE_IF_NULL)
+            val gender: String? = null,
+            @SerializationRule(Rule.EXCLUDE_IF_NULL)
+            var currentVersion: ModelVersion? = null) : HasKey<String>
