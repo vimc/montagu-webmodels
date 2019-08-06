ed51613431547cb05dc8e1883d25d9447a57c371
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-08-06 15:44:11 +0100

    Added disease to ResearchModelDetails

--- src/main/kotlin/org/vaccineimpact/api/models/ResearchModel.kt
+++ src/main/kotlin/org/vaccineimpact/api/models/ResearchModel.kt
@@ -10,11 +10,12 @@ constructor(override val id: String,
             val modellingGroup: String) : HasKey<String>
 
 data class ResearchModelDetails
-@ConstructorProperties("id", "description", "citation", "modellingGroup", "genderSpecific", "gender")
+@ConstructorProperties("id", "description", "citation", "modellingGroup", "disease", "genderSpecific", "gender")
 constructor(override val id: String,
             val description: String,
             val citation: String,
             val modellingGroup: String,
+            var disease: Disease,
             val genderSpecific: Boolean? = null,
             val gender: String? = null,
             var currentVersion: ModelVersion? = null) : HasKey<String>
