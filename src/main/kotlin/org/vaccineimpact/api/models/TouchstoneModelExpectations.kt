4158766848e06076c747cec8eeff130d2d461b5b
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-08-07 16:47:36 +0100

    Added scenarios to TouchstoneModelExpectations

--- src/main/kotlin/org/vaccineimpact/api/models/TouchstoneModelExpectations.kt
+++ src/main/kotlin/org/vaccineimpact/api/models/TouchstoneModelExpectations.kt
@@ -3,8 +3,9 @@ package org.vaccineimpact.api.models
 import java.beans.ConstructorProperties
 
 data class TouchstoneModelExpectations
-@ConstructorProperties("touchstoneVersion", "modellingGroup", "disease", "expectations")
+@ConstructorProperties("touchstoneVersion", "modellingGroup", "disease", "expectations", "scenarios")
 constructor(val touchstoneVersion: String,
             val modellingGroup: String,
             val disease: String,
-            val expectations: OutcomeExpectations)
+            val expectations: OutcomeExpectations,
+            val scenarios: List<String>)
