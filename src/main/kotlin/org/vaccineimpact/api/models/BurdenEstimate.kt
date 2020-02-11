a97335ff9d1d8370e79a90ae76cff28ac64ab2c1
Alex <alex.hill@gmail.com>
2020-02-11 11:56:15 +0000

    remove stochastic estimate

--- src/main/kotlin/org/vaccineimpact/api/models/BurdenEstimate.kt
+++ src/main/kotlin/org/vaccineimpact/api/models/BurdenEstimate.kt
@@ -18,19 +18,6 @@ data class BurdenEstimate(
         val outcomes: Map<String, Float?>
 ) : BurdenEstimateRow
 
-@FlexibleColumns
-data class StochasticBurdenEstimate(
-        val disease: String,
-        val runId: String,
-        val year: Short,
-        val age: Short,
-        val country: String,
-        val countryName: String,
-        val cohortSize: Float,
-        @FlexibleProperty
-        val outcomes: Map<String, Float?>
-) : BurdenEstimateRow
-
 data class BurdenEstimateWithRunId(
         val disease: String,
         val runId: String?,
@@ -45,12 +32,6 @@ data class BurdenEstimateWithRunId(
 {
     constructor(estimate: BurdenEstimate, runId: String?) : this(
             estimate.disease, runId,
-            estimate.year, estimate.age, estimate.country, estimate.countryName,
-            estimate.cohortSize, estimate.outcomes
-    )
-
-    constructor(estimate: StochasticBurdenEstimate) : this(
-            estimate.disease, estimate.runId,
             estimate.year, estimate.age, estimate.country, estimate.countryName,
             estimate.cohortSize, estimate.outcomes
     )
