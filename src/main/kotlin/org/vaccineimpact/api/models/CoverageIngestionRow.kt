202eef54a3e974179ace7af9fc0388a3ceab40b3
Alex <alex.hill@gmail.com>
2020-10-12 16:58:36 +0100

    add CoverageIngestionRow

---
+++ src/main/kotlin/org/vaccineimpact/api/models/CoverageIngestionRow.kt
@@ -0,0 +1,13 @@
+package org.vaccineimpact.api.models
+
+data class CoverageIngestionRow(
+        val vaccine: String,
+        val country: String,
+        val activityType: ActivityType,
+        val gaviSupport: GAVISupportLevel,
+        val year: Int,
+        val ageFirst: Int,
+        val ageLast: Int,
+        val target: Float,
+        val coverage: Float
+)
